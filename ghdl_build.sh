21ff1883e2cca958093ce31dceda20a6669d608a
siluks <slaweksiluk@gazeta.pl>
2018-01-02 12:45:55 +0100

    Add first version of ghdl install script
    Not yet fully proven

---
+++ ghdl_build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# This script builds GHDL with gcc backend
+# TODO parse sources directories from cmd line
+
+# Paramaters
+GHDL_SRC_DIR="$HOME/git/github/ghdl"
+GCC_SRC_DIR="$HOME/sources/gcc-7.1.0"
+INSTALL_PREFIX="$HOME/install_prefix"
+
+echo $GHDL_SRC_DIR
+echo $GCC_SRC_DIR
+echo $INSTALL_PREFIX
+
+# Exit on error
+#set -e
+
+# Prepare ghdl sources
+cd $GHDL_SRC_DIR &&
+mkdir build ; cd build &&
+../configure --with-gcc=$GCC_SRC_DIR --prefix=$INSTALL_PREFIX &&
+make copy-sources
+
+# Dowonload prerequisites for gcc
+cd $GCC_SRC_DIR && ./contrib/download_prerequisites
+
+# Compile ghdl with gcc backend
+cd $GHDL_SRC_DIR/build && mkdir gcc-objs ; cd gcc-objs &&
+$GCC_SRC_DIR/configure --prefix=$INSTALL_PREFIX --enable-languages=c,vhdl --disable-bootstrap --disable-lto --disable-multilib --disable-libssp --disable-libgomp --disable-libquadmath &&
+make -j2 &&
+make install MAKEINFO=true
+
+## Install ghdl libs
+cd $GHDL_SRC_DIR &&
+make ghdllib &&
+make install
+
+
