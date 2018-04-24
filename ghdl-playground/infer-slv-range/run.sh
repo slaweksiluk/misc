cd3d8fe703289a35d5d832f0927abfbadb4b7ae2
Slawomir Siluk <slaweksiluk@gazeta.pl>
2018-04-24 17:27:20 +0200

    Improve ghdl bug reproducer

--- ghdl-playground/infer-slv-range/run.sh
+++ ghdl-playground/infer-slv-range/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-ghdl -a range_addr_decoder.vhd
-ghdl -a range_addr_decoder_tb.vhd
-ghdl -e range_addr_decoder_tb
-./range_addr_decoder_tb
+ghdl -a ghdl_bug.vhd
+ghdl -e tb
+./tb
